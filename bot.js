305f8ca2412aee0f587127042d1acb727b78feba
abdogme <52200948+abdogme@users.noreply.github.com>
2019-06-25 19:12:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,10 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("592959284590149632")
+let channel =     client.channels.get("592816507860418621")
 setInterval(function() {
 channel.send(`play is spam play is spam play is spam`);
 }, 30)
 })
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT__TOKEN);
